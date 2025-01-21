c1939e5d5fc8c99a0e63c9cc0e01ef3ddbcffefd
Anant Joshi <anant.johi2704@gmail.com>
2025-01-21 23:45:38 +0530

    Did all changes

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -270,7 +270,7 @@ const cartItems = useSelector((state) =>
                <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
                <a href="/" style={{textDecoration:'none'}}>
                         <div>
-                    <h3 style={{color:'white'}}>Lush Green</h3>
+                    <h3 style={{color:'white'}}>Green Heaven</h3>
                     <i style={{color:'white'}}>Cultivating Nature's Finest</i>
                     </div>
                     </a>
