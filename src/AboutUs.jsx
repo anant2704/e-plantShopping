65276677ff1916b8f473c1394ec05b6e48475393
Anant Joshi <anant.johi2704@gmail.com>
2025-01-21 23:49:12 +0530

    Did all changes

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -7,17 +7,17 @@ function AboutUs() {
       {/* <h1 className="about-us-heading">About Us</h1> */}
       <p className="about-us-description">Welcome to Green Heaven, where green meets serenity!</p>
       <p className="about-us-content">
-      Welcome to Green Heaven Nursery, where nature thrives and dreams of greenery come to life. At Lush Green, we specialize in cultivating a wide variety of premium plants, from vibrant flowering blooms to lush foliage and exotic species. Our nursery is a sanctuary for plant lovers, offering not only quality greenery but also expert guidance to nurture your passion for gardening. 
+      Welcome to Green Heaven Nursery, where nature thrives and dreams of greenery come to life. At Green Heaven, we specialize in cultivating a wide variety of premium plants, from vibrant flowering blooms to lush foliage and exotic species. Our nursery is a sanctuary for plant lovers, offering not only quality greenery but also expert guidance to nurture your passion for gardening. 
       </p>
       {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
       <p className="about-us-content">
         Our team of experts is dedicated to ensuring that each plant meets our strict standards of quality and care. 
-        Whether you're designing a tranquil garden, enhancing your home decor, or simply seeking a breath of fresh air, Lush Green is your trusted partner. Step into our world, where every plant is nurtured with care, and let us help you create a greener tomorrow.
+        Whether you're designing a tranquil garden, enhancing your home decor, or simply seeking a breath of fresh air, Green Heaven is your trusted partner. Step into our world, where every plant is nurtured with care, and let us help you create a greener tomorrow.
       </p>
       {/* <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
 
       <p className="about-us-content">
-        Join us in our mission to create a greener, healthier world. Visit Lush Green today and experience the 
+        Join us in our mission to create a greener, healthier world. Visit Green Heaven today and experience the 
         beauty of nature right at your doorstep.
       </p>
     </div>
