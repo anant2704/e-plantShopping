119b17dff5377b4457b0e6a6fbf32aae3aebf90f
Anant Joshi <anant.johi2704@gmail.com>
2025-01-21 23:47:00 +0530

    Did all changes

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -5,9 +5,9 @@ function AboutUs() {
   return (
     <div className="about-us-container">
       {/* <h1 className="about-us-heading">About Us</h1> */}
-      <p className="about-us-description">Welcome to Lush Green, where green meets serenity!</p>
+      <p className="about-us-description">Welcome to Green Heaven, where green meets serenity!</p>
       <p className="about-us-content">
-      Welcome to Lush Green Nursery, where nature thrives and dreams of greenery come to life. At Lush Green, we specialize in cultivating a wide variety of premium plants, from vibrant flowering blooms to lush foliage and exotic species. Our nursery is a sanctuary for plant lovers, offering not only quality greenery but also expert guidance to nurture your passion for gardening. 
+      Welcome to Green Heaven Nursery, where nature thrives and dreams of greenery come to life. At Lush Green, we specialize in cultivating a wide variety of premium plants, from vibrant flowering blooms to lush foliage and exotic species. Our nursery is a sanctuary for plant lovers, offering not only quality greenery but also expert guidance to nurture your passion for gardening. 
       </p>
       {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
       <p className="about-us-content">
